b658c57efe1d48a1bd7a48f1d670e49f850a8c7f
Kirill Kosykh <triplesonicslash@list.ru>
2023-02-01 08:05:11 +0300

    Initial commit

---
+++ src/main/java/com/colossus/ecomerce/config/MyDataRestConfig.java
@@ -0,0 +1,31 @@
+package com.colossus.ecomerce.config;
+
+import com.colossus.ecomerce.entity.Product;
+import com.colossus.ecomerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.POST};
+
+        //disable HTTP methods for Product: PUT, DELETE, POST
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //disable HTTP methods for ProductCategory: PUT, DELETE, POST
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+    }
+
+}
